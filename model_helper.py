1b4e94e475bf4b1946ff7500e8bbb83ebd374993
kavindu-specs <kavinduprabodya@gmail.com>
2025-06-06 12:30:21 +0530

    add custome template

--- model_helper.py
+++ model_helper.py
@@ -1,16 +1,26 @@
 from langchain.chat_models import init_chat_model
+from langchain_openai import ChatOpenAI
 import os 
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
-async def call_open_ai_model(question: str) -> str:
-  model = init_chat_model("gpt-4o-mini", model_provider="openai",temperature=0.2)
+async def call_open_ai_model(question: str,llm_model: str, style: str,domain:str, lan:str) -> str:
+  model = ChatOpenAI(
+               model_name=llm_model,  # preferred and correct
+               temperature=0.1
+             )
   
-  system_template = """You are a knowledgeable, concise, and helpful assistant. Answer questions clearly, accurately friendly. If you don’t know the answer, 
-                    say so honestly. Do not make up information.
-                   """
+  style="UK english in a polite way"
+  
+  system_template = f"""
+                      You are a knowledgeable, concise, and helpful assistant working in the domain of {domain}.
+                      You respond in {lan}, following this answering style: {style}.
+                      Answer questions clearly, accurately, and in a friendly tone. 
+                      If you don’t know the answer, say so honestly. Do not make up information.
+                          """
   
   prompt = ChatPromptTemplate.from_messages(
     [("system", system_template), ("user", "{user_input}")]
